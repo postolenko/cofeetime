94562fed0aa3bf2ee2c73dadcef12d23465911c9
postolenko <denis.postolenko@gmail.com>
2022-01-19 20:12:30 +0200

    Coffeetime

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,3 @@
-var w = window,
-d = document,
-e = d.documentElement,
-g = d.getElementsByTagName('body')[0],
-bodyWidth = w.innerWidth || e.clientWidth || g.clientWidth;
-
-
-$(window).load(function() {
-
-
-
-});
-
-$(window).resize(function() {
-
-
-
-});
-
-$(document).scroll(function() {
-
-
-
-});
-
 $(document).ready(function() {
 
     if( $(".slider").length > 0 ) {
